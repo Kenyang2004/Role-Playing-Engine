defff4203ae97fd6a9b7851d0910bbc6e27c89c3
Ken <jellykenyang@gmail.com>
2026-01-29 20:02:22 -0800

    Add Character abstract base class

---
+++ src/rpg/characters/Character.java
@@ -0,0 +1,63 @@
+package rpg.characters;
+
+public abstract class Character {
+    protected String name;
+    protected int maxHealth;
+    protected int currentHealth;
+    protected int attack;
+    protected int defense;
+    protected int level;
+
+
+    //Constructor
+    public Character(String name, int maxHealth, int currentHealth, int attack, int defense, int level) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = currentHealth;
+        this.attack = attack;
+        this.defense = defense;
+        this.level = level;
+
+    }
+
+    //Method
+    public boolean isAlive() {
+        return currentHealth > 0;
+    }
+
+    public void takeDamage(int amount) {
+        currentHealth -= amount;
+        if (currentHealth < 0) {
+            currentHealth = 0; //Reset to 0 
+        }
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public abstract void levelUp();
+    
+
+
+}
